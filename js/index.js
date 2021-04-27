b3f603420b687efb6960598e9660a8e8e50ebd5b
LittleDC <1187061276@qq.com>
2021-04-27 17:03:10 +0800

    fix some bugs

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('nav li')[0].ontouchstart = () => {
+    location.href = './join_room.html'
+}
+$('nav li')[1].ontouchstart = () => {
+    location.href = './create_room.html'
+}
+$('nav li')[2].ontouchstart = () => {
+    location.href = './enter_room.html'
+}
+$('nav li')[3].ontouchstart = () => {
+    location.href = './statistics.html'
+}
