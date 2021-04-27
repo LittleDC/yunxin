9e7f6cb30950baf1b9a97d3a171c2786cdf958e6
LittleDC <1187061276@qq.com>
2021-04-27 15:57:20 +0800

    profile done, start setting

---
+++ js/profile.js
@@ -0,0 +1,5 @@
+// console.log(1);
+// console.log($('#back')[0]);
+$('#back')[0].ontouchstart = () => {
+    location.href = "../user.html"
+}
---
+++ js/user.js
@@ -0,0 +1,8 @@
+console.log(3);
+$('#profile')[0].ontouchstart = () => {
+    location.href = '../profile.html'
+}
+
+$('#setting')[0].ontouchstart = () => {
+    location.href = '../setting.html'
+}
