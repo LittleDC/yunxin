06dfc683a7fb544a61e69d07d80691375f79f95d
LittleDC <1187061276@qq.com>
2021-04-30 23:39:09 +0800

    some bugs of this done & invite_box display finish

--- js/create_room.js
+++ js/create_room.js
@@ -6,6 +6,18 @@ $('#create')[0].addEventListener('click', () => {
     location.href = './studying_room.html'
 })
 
-$('#invite')[0].addEventListener('click', () => {
+$('#invite')[0].addEventListener('click', function () {
     console.log('invite bottom');
+    // $(this).toggleClass('none');
+    $('.invite_box').toggleClass('none')
+    // console.log($(this));
 })
+
+// $('#invite')[0].onclick = () => {
+//     console.log(this);
+//     console.log($('#invite')[0]);
+// }
+
+// $('#invite').click(function () {
+//     console.log(this);
+// })
